7ab3e20b6735667c1a7f706f4d7739c750881acf
Yilin Chen <sticnarf@gmail.com>
2019-04-26 17:06:21 +0800

    Add documents for breakpad-client
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- breakpad-client/src/lib.rs
+++ breakpad-client/src/lib.rs
@@ -1,10 +1,17 @@
-use breakpad_sys::{register_handler_from_fd, register_handler_from_path, DescriptorInfo};
+//! A Rust library to use Google breakpad client.
+
+use breakpad_sys::{register_handler_from_fd, register_handler_from_path};
 use std::ffi::CStr;
 use std::os::raw::c_void;
 use std::os::unix::ffi::OsStrExt;
 use std::os::unix::io::{FromRawFd, RawFd};
 use std::path::PathBuf;
 
+pub use breakpad_sys::DescriptorInfo;
+
+/// Describes where to save the minidump
+///
+/// A directory or an opened file descriptor is supported
 pub enum MinidumpDescriptor {
     Directory(PathBuf),
     Fd(RawFd),
@@ -22,36 +29,59 @@ impl FromRawFd for MinidumpDescriptor {
     }
 }
 
+/// Defines the behavior when the program crashes
 pub trait ExceptionHandler {
     type Context: 'static;
 
-    fn descriptor(&self) -> &MinidumpDescriptor;
-
+    /// Returns the context you want to register.
+    /// This context will be boxed to the heap and registered.
     fn context(self) -> Self::Context;
 
-    fn filter_callback(_context: &'static mut Self::Context) -> bool {
+    /// A callback function to run before Breakpad performs any substantial
+    /// processing of an exception. It is called before writing
+    /// a minidump. `context` is the object supplied by the `context` function.
+    ///
+    /// If it returns true, Breakpad will continue processing,
+    /// attempting to write a minidump.  If it returns false,
+    /// Breakpad  will immediately report the exception as unhandled without
+    /// writing a minidump, allowing another handler the opportunity to handle it.
+    ///
+    /// The default implementation just returns `true`.
+    #[allow(unused_variables)]
+    fn filter_callback(context: &'static mut Self::Context) -> bool {
         true
     }
 
+    /// A callback function to run after the minidump has been written.
+    /// `descriptor` contains the file descriptor or file path containing the
+    /// minidump. `context` is the object supplied by the `context` function.
+    /// `succeeded` indicates whether a minidump file was successfully written.
+    ///
+    /// If an exception occurred and the callback returns true, Breakpad will
+    /// treat the exception as fully-handled, suppressing any other handlers from
+    /// being notified of the exception.  If the callback returns false, Breakpad
+    /// will treat the exception as unhandled, and allow another handler to handle
+    /// it. If there are no other handlers, Breakpad will report the exception to
+    /// the system as unhandled, allowing a debugger or native crash dialog the
+    /// opportunity to handle the exception.  Most callback implementations
+    /// should normally return the value of `succeeded`, or when they wish to
+    /// not report an exception of handled, false.  Callbacks will rarely want to
+    /// return true directly (unless `succeeded` is true).
+    ///
+    /// The default implementation just returns `succeeded`.
+    #[allow(unused_variables)]
     fn minidump_callback(
-        _descriptor: DescriptorInfo,
-        _context: &'static mut Self::Context,
+        descriptor: DescriptorInfo,
+        context: &'static mut Self::Context,
         succeeded: bool,
     ) -> bool {
         succeeded
     }
 }
 
-struct RustContext<H: ExceptionHandler> {
-    filter_callback: fn(&'static mut H::Context) -> bool,
-    minidump_callback: fn(DescriptorInfo, &'static mut H::Context, bool) -> bool,
-    ctx: *mut H::Context,
-}
-
 extern "C" fn filter_callback_wrapper<H: ExceptionHandler>(ctx: *mut c_void) -> bool {
-    let context = unsafe { Box::from_raw(ctx as *mut RustContext<H>) };
-    let ctx = unsafe { Box::from_raw(context.ctx) };
-    (context.filter_callback)(Box::leak(ctx))
+    let context = unsafe { Box::from_raw(ctx as *mut H::Context) };
+    H::filter_callback(Box::leak(context))
 }
 
 extern "C" fn minidump_callback_wrapper<H: ExceptionHandler>(
@@ -59,25 +89,14 @@ extern "C" fn minidump_callback_wrapper<H: ExceptionHandler>(
     ctx: *mut c_void,
     succeeded: bool,
 ) -> bool {
-    let context = unsafe { Box::from_raw(ctx as *mut RustContext<H>) };
-    let ctx = unsafe { Box::from_raw(context.ctx) };
-    (context.minidump_callback)(info, Box::leak(ctx), succeeded)
+    let context = unsafe { Box::from_raw(ctx as *mut H::Context) };
+    H::minidump_callback(info, Box::leak(context), succeeded)
 }
 
-pub fn register<H: ExceptionHandler>(handler: H) {
-    macro_rules! rust_context {
-        () => {{
-            let ctx = Box::into_raw(Box::new(handler.context()));
-            let context: Box<RustContext<H>> = Box::new(RustContext {
-                filter_callback: H::filter_callback,
-                minidump_callback: H::minidump_callback,
-                ctx,
-            });
-            Box::into_raw(context) as *mut c_void
-        }};
-    }
-
-    match handler.descriptor() {
+/// Register an `ExceptionHandler` with the given descriptor.
+pub fn register<H: ExceptionHandler>(descriptor: impl Into<MinidumpDescriptor>, handler: H) {
+    let ctx = Box::into_raw(Box::new(handler.context()));
+    match descriptor.into() {
         MinidumpDescriptor::Directory(path) => {
             let c_path = CStr::from_bytes_with_nul(path.as_os_str().as_bytes())
                 .expect("Path is not null terminated")
@@ -87,16 +106,16 @@ pub fn register<H: ExceptionHandler>(handler: H) {
                     c_path,
                     Some(filter_callback_wrapper::<H>),
                     Some(minidump_callback_wrapper::<H>),
-                    rust_context!(),
+                    ctx as *mut c_void,
                 );
             }
         }
         MinidumpDescriptor::Fd(fd) => unsafe {
             register_handler_from_fd(
-                *fd,
+                fd,
                 Some(filter_callback_wrapper::<H>),
                 Some(minidump_callback_wrapper::<H>),
-                rust_context!(),
+                ctx as *mut c_void,
             );
         },
     }
